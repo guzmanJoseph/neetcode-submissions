4b08d8a75d39d1d83cfaf164d793ea1a7d701e94
Joseph Guzman <joseph.guzman05@hotmail.com>
2026-08-24 21:15:36 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,26 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return None
+        hash_map = {}
+
+        def dfs(current):
+            if current in hash_map:
+                return hash_map[current]
+
+            copy = Node(current.val)
+            hash_map[current] = copy
+
+            for neighbor in current.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+        
+            return copy
+        return dfs(node)
